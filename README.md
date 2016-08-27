32149ae3c9b2a2a6cb4dcd94f10e1555eb8b844b
aydin abdnia <aydin.abdnia@gmail.com>
2016-08-27 17:10:08 +0430

    remove broken packages and push

--- README.md
+++
@@ -1,2 +0,0 @@
-# softcute
-broken ui framework 
--- servertest.php
+++
@@ -1,2 +0,0 @@
-<?php  $_SERVER['SERVER_NAME'].'/files/_thumb_876022559823path.jpg'; ?>
-<img src="<?php echo 'http://'.$_SERVER['HTTP_HOST'].'/softcute'; ?>/images/EE.jpg" />
